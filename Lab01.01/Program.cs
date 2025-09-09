f117b6c8378b175e58b7f29f6afb0cb97d7a3ff4
CoF-Luke <vladpilot.new@gmail.com>
2025-09-09 15:24:36 +0300

    Добавьте файлы проекта.

---
+++ Lab01.01/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Predefined types and their extreme values:");
+
+Console.WriteLine("\n\tOrdinary numbers:\n");
+
+Console.WriteLine($"\t\tbyte    : {byte.MinValue} ... {byte.MaxValue}");
+Console.WriteLine($"\t\tsbyte   : {sbyte.MinValue} ... {sbyte.MaxValue}");
+Console.WriteLine($"\t\tshort   : {short.MinValue} ... {short.MaxValue}");
+Console.WriteLine($"\t\tushort  : {ushort.MinValue} ... {ushort.MaxValue}");
+Console.WriteLine($"\t\tint     : {int.MinValue} ... {int.MaxValue}");
+Console.WriteLine($"\t\tuint    : {uint.MinValue} ... {uint.MaxValue}");
+Console.WriteLine($"\t\tlong    : {long.MinValue} ... {long.MaxValue}");
+Console.WriteLine($"\t\tulong   : {ulong.MinValue} ... {ulong.MaxValue}");
+
+Console.WriteLine("\n\tFloating-point numbers:\n");
+
+Console.WriteLine($"\t\tfloat   : {float.MinValue} ... {float.MaxValue}");
+Console.WriteLine($"\t\tdouble  : {double.MinValue} ... {double.MaxValue}");
+Console.WriteLine($"\t\tdecimal : {decimal.MinValue} ... {decimal.MaxValue}");
+
+Console.WriteLine("\n\tOthers:\n");
+
+Console.WriteLine($"\t\tchar    : {(int)char.MinValue} ... {(int)char.MaxValue}");
+Console.WriteLine($"\t\tbool    : {bool.FalseString} / {bool.TrueString}");
---
+++ Lab01.02/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Write number A: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Write number B: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("\n");
+
+        Rectangle Rec = new(a, b);
+
+        Console.WriteLine($"Area is {Rec.Area}; Perimeter is {Rec.Perimeter}");
+
+
+    }
+}
+
+class Rectangle
+{
+    double sideA, sideB;
+
+    public Rectangle(double sideA, double sideB)
+    {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    private double CalculateArea()
+    {
+        return sideA * sideB;
+    }
+
+    private double CalculatePerimeter()
+    {
+        return (sideA + sideB) * 2;
+    }
+
+    public double Area
+    {
+        get
+        {
+            return CalculateArea();
+        }
+    }
+
+    public double Perimeter
+    {
+        get
+        {
+            return CalculatePerimeter();
+        }
+    }
+}
